6c367b541be1284717bd0ca4849c235731568471
João Pedro Barbosa Duarte Ferreira <141972472+CronusLDX@users.noreply.github.com>
2024-06-10 20:43:16 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,37 +1,27 @@
 import axios from "axios";
-import langColors from '../styles/LangColors';
+
+import langColors from '../styles/LangColors'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
+ export const getUser = async (login) => api.get(`/users/${login}`);
 
-export const getUser = async (login) => api.get(`/users/${login}`);
-
-export const getRepos = async (login) => api.get(`/users/${login}/repos`);
+ export const getRepos = async (login)=> api.get(`/users/${login}/repos`)
 
 export default api;
 
 export const getLangsFrom = (repositories) => {
-  if (!Array.isArray(repositories)) {
-    throw new TypeError("Expected repositories to be an array");
-  }
-
-  let getLanguage = repositories
-    .map((repository) => repository.language)
-    .reduce((data, language) => ({
-      ...data,
-      [language]: (data[language] || 0) + 1,
-    }), {});
-
-  delete getLanguage.null;
-
-  getLanguage = Object.keys(getLanguage)
-    .map((language) => ({
-      name: language,
-      count: getLanguage[language],
-      color: langColors[language.toLowerCase()]
-    }))
-    .sort((a, b) => b.count - a.count);
-
+  let getLanguage = repositories.map((repository)=> repository.language).reduce((data,language)=>({...data,[language]: (data[language] || 0 ) + 1,}),[null])
+ 
+ delete getLanguage.null
+ // Serve para criar um novo array com todas as chaves do objeto que temos
+   getLanguage = Object.keys(getLanguage)
+   .map((language)=>({
+     name: language,
+     count: getLanguage[language],
+     color: langColors[language.toLowerCase()]
+   }))
+   .sort((a,b)=> b.count - a.count)
   return getLanguage;
-};
+}
